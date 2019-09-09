ef095866e9bee0fde3972398e849f51fd534b96d
Rei M <r.clarkemitchell@gmail.com>
2019-09-09 22:02:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,12 +2,12 @@ def roll_call_dwarves(source_array)
  source_array.each_with_index{|val, index| p "#{index + 1} #{val}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (source_array)
+ p source_array.map {|val| "#{val.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(source_array)
+   source_array.any? {|val| val.length > 4}
 end
 
 def find_the_cheese# code an argument here
